1289fcc93c139b981a6e8bb33a94683d4a5b5e29
KseniyaDylevich <118763142+KseniyaDylevich@users.noreply.github.com>
2022-12-11 00:23:43 +0300

    Update Задание4.py

--- Задание4.py
+++ Задание4.py
@@ -1,6 +1,3 @@
-#функция кодирования работает, декодирование выдает ошибку(((
-#пока не могу разобраться
-
 def encode(decoded_string):
     encoded_string = ''
     count = 1
@@ -14,8 +11,6 @@ def encode(decoded_string):
             count = 1
         if i == len(decoded_string)-1:
             encoded_string = encoded_string + str(count) + char
-
-            #encoded_string = encoded_string + str(count) + char
     return encoded_string
 
 
@@ -26,26 +21,27 @@ def decode(encoded_string):
         if i.isdigit():
             char_amount += i
         else:
-            #decoded_string += encoded_string[i] * char_amount
-            #encoded_string1 = (i * char_amount)
             decoded_string += str(i * int(char_amount))
-        char_amount = 0
+            char_amount = ''
     return decoded_string
 
 
-# with open('encode.txt', 'w') as data:
-#     data.write('sssssssssssssssssqqqqqqqqqqqqqqqqqDDDDDDDDDDDDDDDDDDDD')
+with open('decode.txt', 'w') as data:
+    data.write('sssssssssssssssssqqqqqqqqqqqqqqqqqDDDDDDDDDDDDDDDDDDDD')
 
-#with open('encode.txt', 'r') as data:
-  #  string = data.readline()
-
-
-# with open('encode.txt', 'r') as file:
-#     decoded_string = file.read()
+with open('decode.txt', 'r') as data:
+    string = data.readline()
 
 with open('decode.txt', 'r') as file:
-    encoded_string = file.read()
+    decoded_string = file.read()
 
 with open('encode.txt', 'w') as file:
-    decoded_string = decode(encoded_string)
-    file.write(decoded_string)
+    file.write(encode(decoded_string))
+
+with open('encode.txt', 'r') as file:
+    encoded_string = file.read()
+
+with open('decode2.txt', 'w') as data:
+    data.write(decode(encoded_string))
+
+
